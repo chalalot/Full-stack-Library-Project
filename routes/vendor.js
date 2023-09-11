adb284a9d0a6bc849a52a6deb01891ca11d39c1a
Bunchango <119033041+Bunchango@users.noreply.github.com>
2023-09-11 18:35:39 +0700

    add error message for create product form

--- routes/vendor.js
+++ routes/vendor.js
@@ -25,7 +25,7 @@ router.get("/view-products", checkAuthenticated, async (req, res) => {
 
 router.get("/add-products", checkAuthenticated, (req, res) => {
   // Render the form
-  res.render("vendor/add-products.ejs");
+  res.render("vendor/add-products.ejs", { error: "" });
 });
 
 router.get("/view-products/:id", checkAuthenticated, async (req, res) => {
@@ -45,23 +45,31 @@ router.get("/view-products/:id", checkAuthenticated, async (req, res) => {
   }
 }); // Render product page from product id
 
-router.post("/add-products", upload.single("image"), async (req, res) => {
-  try {
-    let product = new Product.Product({
-      name: req.body.name,
-      vendor: req.user._id,
-      price: parseFloat(req.body.price), // Might have to parse to int
-      image: req.file.path,
-      description: req.body.description,
-    });
+router.post(
+  "/add-products",
+  checkAuthenticated,
+  upload.single("image"),
+  async (req, res) => {
+    try {
+      let product = new Product.Product({
+        name: req.body.name,
+        vendor: req.user._id,
+        price: parseFloat(req.body.price),
+        image: req.file.path,
+        description: req.body.description,
+      });
 
-    await product.save();
-    res.redirect(`/vendor/view-products/${product._id}`); // Redirect to the product page
-  } catch (e) {
-    // Redirect and show error
-    console.log(e);
-    res.render("vendor/add-products.ejs");
-  }
-});
+      await product.save();
+      res.redirect(`/vendor/view-products/${product._id}`); // Redirect to the product page
+    } catch (e) {
+      // Redirect and show error
+      req.flash("error", e.message);
+      const regex = /name: (.*)/;
+      const match = regex.exec(e.message);
+      const errorMessage = match[1];
+      res.render("vendor/add-products.ejs", { error: errorMessage });
+    }
+  },
+);
 
 module.exports = router;
