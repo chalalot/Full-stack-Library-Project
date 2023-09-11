57236927bc4214c4a8f6f7a2e6a7d84bc713a9b7
Bunchango <119033041+Bunchango@users.noreply.github.com>
2023-09-11 12:05:18 +0700

    finish product schema

--- models/product.js
+++ models/product.js
@@ -1,25 +1,76 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
-const productSchema = new Schema({
-  seller: {
+const productSchema = Schema({
+  name: {
+    type: String,
+    required: true,
+    minLength: [10, "Name length must be between 10 and 20"],
+    maxLength: [20, "Name length must be between 10 and 20"],
+  },
+  vendor: {
     type: Schema.Types.ObjectId,
-    ref: "User",
+    ref: "Vendor",
     required: true,
   },
-  name: {
+  price: {
+    type: Number,
+    required: true,
+    min: [0, "Price must be higher than 0"],
+  },
+  image: {
     type: String,
     required: true,
   },
   description: {
     type: String,
     required: true,
+    maxLength: [500, "Description character count must be below 500"],
   },
-  price: {
-    type: Number,
-    required: true,
-  },
-  image: {},
 });
 
-module.exports = mongoose.model("Product", productSchema);
+const orderSchema = Schema({
+  customer: {
+    type: Schema.Types.ObjectId,
+    ref: "Customer",
+    required: true,
+  },
+  products: [
+    {
+      product: {
+        type: Schema.Types.ObjectId,
+        ref: "Product",
+        required: true,
+      },
+    },
+  ],
+  address: {
+    // Get directly users address
+    type: String,
+    required: true,
+  },
+  orderDate: {
+    type: Date,
+    default: Date.now,
+  },
+  status: {
+    type: String,
+    enum: ["active", "delivered", "canceled"],
+    default: "active",
+  },
+});
+
+// Calculate total price
+orderSchema.virtual("total").get(function () {
+  return this.products.reduce((total, product) => {
+    return total + product.price;
+  }, 0);
+});
+
+const Product = mongoose.model("Product", productSchema);
+const Order = mongoose.model("Order", orderSchema);
+
+module.exports = {
+  Product,
+  Order,
+};
