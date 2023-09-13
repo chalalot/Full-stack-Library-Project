8e978fc127a9b6e2c57bc8622c73fc30134fde5b
Bunchango <119033041+Bunchango@users.noreply.github.com>
2023-09-13 20:59:59 +0700

    add hub name to profile

--- routes/profile.js
+++ routes/profile.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const User = require("./../models/user.js");
+const Hub = require("./../models/hub.js");
 const upload = require("./../config/multer.js");
 
 router.post("/change-avatar", upload.single("image"), async (req, res) => {
@@ -46,8 +47,14 @@ router.get("/vendor-profile", checkAuthenticated, (req, res) => {
   res.render("profiles/vendor-profile.ejs", { user: req.user });
 });
 
-router.get("/shipper-profile", checkAuthenticated, (req, res) => {
-  res.render("profiles/shipper-profile.ejs", { user: req.user });
+router.get("/shipper-profile", checkAuthenticated, async (req, res) => {
+  try {
+    const hub = await Hub.findById(req.user.hub);
+    res.render("profiles/shipper-profile.ejs", { user: req.user, hub: hub });
+  } catch (e) {
+    console.log(e);
+    res.redirect("/");
+  }
 });
 
 module.exports = router;
