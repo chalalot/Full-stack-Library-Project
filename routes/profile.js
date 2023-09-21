eae5881c79f293b4c4feb39ed3391d2b8e291922
Bunchango <119033041+Bunchango@users.noreply.github.com>
2023-09-21 20:41:44 +0700

    fix bug when log out to different user go back to original page different profile

--- routes/profile.js
+++ routes/profile.js
@@ -39,21 +39,19 @@ function checkAuthenticated(req, res, next) {
   res.redirect("/checkin/login");
 }
 
-router.get("/customer-profile", checkAuthenticated, (req, res) => {
-  res.render("profiles/customer-profile.ejs", { user: req.user });
-});
-
-router.get("/vendor-profile", checkAuthenticated, (req, res) => {
-  res.render("profiles/vendor-profile.ejs", { user: req.user });
-});
-
-router.get("/shipper-profile", checkAuthenticated, async (req, res) => {
-  try {
-    const hub = await Hub.findById(req.user.hub);
-    res.render("profiles/shipper-profile.ejs", { user: req.user, hub: hub });
-  } catch (e) {
-    console.log(e);
-    res.redirect("/");
+router.get("/profile", checkAuthenticated, async (req, res) => {
+  if (req.user.__t === "Vendor") {
+    res.render("profiles/vendor-profile.ejs", { user: req.user });
+  } else if (req.user.__t === "Shipper") {
+    try {
+      const hub = await Hub.findById(req.user.hub);
+      res.render("profiles/shipper-profile.ejs", { user: req.user, hub: hub });
+    } catch (e) {
+      console.log(e);
+      res.redirect("/");
+    }
+  } else if (req.user.__t === "Customer") {
+    res.render("profiles/customer-profile.ejs", { user: req.user });
   }
 });
 
